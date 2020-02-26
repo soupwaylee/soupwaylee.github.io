d9f3edbca161edbc638162ec8f70b61a646462dc
Stefan Su <stfn.su@live.de>
2020-02-26 11:23:20 +0100

    Fix configuration file

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,55 +4,57 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-export const siteMetadata = {
-  title: `Stefan Su`,
-  // siteUrl: `https://www.stefansu.com`,
-  description: `A blog with ramblings on code, computer science, cooking and how to become a liver of life`,
-  menuLinks: [
-    {
-      name: '/home',
-      link: '/'
-    },
-    {
-      name: '/writing',
-      link: '/writing/'
-    },
-    {
-      name: '/about',
-      link: '/about/'
-    },
-    {
-      name: '/resume',
-      link: '/resume/'
-    }
-  ]
-};
-export const plugins = [
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `markdown-pages`,
-      path: `${__dirname}/src/markdown-pages`,
-    },
+module.exports = {
+  siteMetadata: {
+    title: `Stefan Su`,
+    // siteUrl: `https://www.stefansu.com`,
+    description: `A blog with ramblings on code, computer science, cooking and how to become a liver of life`,
+    menuLinks: [
+      {
+        name:'/home',
+        link:'/'
+      },
+      {
+        name:'/writing',
+        link:'/writing/'
+      },
+      {
+        name:'/about',
+        link:'/about/'
+      },
+      {
+        name:'/resume',
+        link:'/resume/'
+      }
+    ]
   },
-  {
-    resolve: `gatsby-transformer-remark`,
-    options: {
-      plugins: [
-        {
-          resolve: `gatsby-remark-katex`,
-          options: {
-            // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-            strict: `ignore`
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-katex`,
+            options: {
+              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+              strict: `ignore`
+            }
           }
-        }
-      ],
-    }
-  },
-  {
-    resolve: `gatsby-plugin-typography`,
-    options: {
-      pathToConfigModule: `src/utils/typography`,
+        ],
+      }
     },
-  },
-];
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ]
+}
